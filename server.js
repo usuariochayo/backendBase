a1955573e4e2bcc012d3993ca7d62bab51318ac3
usuariochayo <96098830+usuariochayo@users.noreply.github.com>
2022-12-22 20:12:18 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const messagesRouter = require('./routes/messages')
+
+class Server{
+    constructor(){
+        this.app = express()
+        this.paths = {
+            messages: "/api/v1/messages"
+        }
+        this.routes()
+    
+    }
+
+    routes(){
+        this.app.use(this.paths.messages, messagesRouter)
+    }
+
+    listen(){
+        this.app.listen(process.env.PORT, ()=> {
+            console.log(process.env.PORT);
+        })
+    }
+}
